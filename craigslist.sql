6035e34c4ccd5c2b2f3a825af3e9220d906fee31
Giovanni Rufino <giovannirufino@gmail.com>
2021-06-19 08:18:03 -0400

    craiglist done

---
+++ craigslist.sql
@@ -0,0 +1,36 @@
+CREATE TABLE "Regions" (
+  "id" SERIAL PRIMARY KEY,
+  "city" varchar(20)
+);
+
+CREATE TABLE "Users" (
+  "id" SERIAL PRIMARY KEY,
+  "user_name" varchar(20) UNIQUE,
+  "password" varchar(20),
+  "date_joined" timestamp,
+  "pref_region" int
+);
+
+CREATE TABLE "Catergories" (
+  "id" SERIAL PRIMARY KEY,
+  "category" varchar(20)
+);
+
+CREATE TABLE "Posts" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar(15),
+  "text" text,
+  "user" int,
+  "post_loc" varchar(20),
+  "post_region" int,
+  "date_posted" timestamp,
+  "post_cat" int
+);
+
+ALTER TABLE "Users" ADD FOREIGN KEY ("pref_region") REFERENCES "Regions" ("id");
+
+ALTER TABLE "Posts" ADD FOREIGN KEY ("user") REFERENCES "Users" ("id");
+
+ALTER TABLE "Posts" ADD FOREIGN KEY ("post_region") REFERENCES "Regions" ("id");
+
+ALTER TABLE "Posts" ADD FOREIGN KEY ("post_cat") REFERENCES "Catergories" ("id");
